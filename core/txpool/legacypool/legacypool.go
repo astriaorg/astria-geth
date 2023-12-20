02c2e8cbdfcaa27b4cb0265096cb44a64e376fd4
lightclient <14004106+lightclient@users.noreply.github.com>
2023-12-20 09:23:48 -0700

    cmd/devp2p: update eth/snap protocol test suites for PoS (#28340)
    Here we update the eth and snap protocol test suites with a new test chain,
    created by the hivechain tool. The new test chain uses proof-of-stake. As such,
    tests using PoW block propagation in the eth protocol are removed. The test suite
    now connects to the node under test using the engine API in order to make it
    accept transactions. 
    
    The snap protocol test suite has been rewritten to output test descriptions and
    log requests more verbosely.
    
    ---------
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- core/txpool/legacypool/legacypool.go
+++ core/txpool/legacypool/legacypool.go
@@ -1065,6 +1065,7 @@ func (pool *LegacyPool) Add(txs []*types.Transaction, local, sync bool) []error 
 		// in transactions before obtaining lock
 		if err := pool.validateTxBasics(tx, local); err != nil {
 			errs[i] = err
+			log.Trace("Discarding invalid transaction", "hash", tx.Hash(), "err", err)
 			invalidTxMeter.Mark(1)
 			continue
 		}
