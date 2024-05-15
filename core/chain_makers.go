fa7eeebad89b9e96d51a5e9457bd8c0acb8e7b69
Bharath <vedabharath12345@gmail.com>
2024-05-15 22:00:09 +0530

    fix tests

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -115,6 +115,7 @@ func (b *BlockGen) addTx(bc *BlockChain, vmConfig vm.Config, tx *types.Transacti
 		b.SetCoinbase(common.Address{})
 	}
 	b.statedb.SetTxContext(tx.Hash(), len(b.txs))
+	fmt.Printf("addTx(): Coinbase is %s\n", b.header.Coinbase.String())
 	receipt, err := ApplyTransaction(b.cm.config, bc, &b.header.Coinbase, b.gasPool, b.statedb, b.header, tx, &b.header.GasUsed, vmConfig)
 	if err != nil {
 		panic(err)
