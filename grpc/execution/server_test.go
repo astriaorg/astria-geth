8689eece656357ec48367a56434e9a05e6404acb
Bharath <vedabharath12345@gmail.com>
2024-05-18 23:22:07 +0530

    add test case for deposit tx

--- grpc/execution/server_test.go
+++ grpc/execution/server_test.go
@@ -3,6 +3,7 @@ package execution
 import (
 	astriaGrpc "buf.build/gen/go/astria/execution-apis/grpc/go/astria/execution/v1alpha2/executionv1alpha2grpc"
 	astriaPb "buf.build/gen/go/astria/execution-apis/protocolbuffers/go/astria/execution/v1alpha2"
+	primitivev1 "buf.build/gen/go/astria/primitives/protocolbuffers/go/astria/primitive/v1"
 	sequencerblockv1alpha1 "buf.build/gen/go/astria/sequencerblock-apis/protocolbuffers/go/astria/sequencerblock/v1alpha1"
 	"bytes"
 	"context"
@@ -48,10 +49,26 @@ func generateMergeChain(n int, merged bool) (*core.Genesis, []*types.Block) {
 		config.TerminalTotalDifficultyPassed = true
 		engine = beaconConsensus.NewFaker()
 	}
+
+	bridgeAddressKey, err := crypto.GenerateKey()
+	if err != nil {
+		panic(err)
+	}
+	bridgeAddress := crypto.PubkeyToAddress(bridgeAddressKey.PublicKey)
+
 	config.AstriaRollupName = "astria"
 	config.AstriaSequencerInitialHeight = 10
 	config.AstriaCelestiaInitialHeight = 10
 	config.AstriaCelestiaHeightVariance = 10
+	config.AstriaBridgeAddressConfigs = []params.AstriaBridgeAddressConfig{
+		{
+			BridgeAddress:  bridgeAddress.Bytes(),
+			StartHeight:    2,
+			AssetDenom:     "0000000000000000000000000000nria",
+			AssetPrecision: 18,
+			Erc20Asset:     nil,
+		},
+	}
 
 	astriaFeeCollectors := make(map[uint32]common.Address)
 	astriaFeeCollectors[1] = common.HexToAddress("0x9a9070028361F7AAbeB3f2F2Dc07F82C4a98A02a")
@@ -543,6 +560,178 @@ func TestExecutionServiceServerV1Alpha2_ExecuteBlockAndUpdateCommitment(t *testi
 	}
 
 	if bytes.Compare(softBlock.Hash().Bytes(), executeBlockRes.Hash) != 0 {
+		t.Fatalf("Soft Block Hashes do not match")
+	}
+	if bytes.Compare(softBlock.ParentHash.Bytes(), executeBlockRes.ParentBlockHash) != 0 {
+		t.Fatalf("Soft Block Parent Hashes do not match")
+	}
+	if softBlock.Number.Uint64() != uint64(executeBlockRes.Number) {
+		t.Fatalf("Soft Block Numbers do not match")
+	}
+
+	if bytes.Compare(firmBlock.Hash().Bytes(), executeBlockRes.Hash) != 0 {
+		t.Fatalf("Firm Block Hashes do not match")
+	}
+	if bytes.Compare(firmBlock.ParentHash.Bytes(), executeBlockRes.ParentBlockHash) != 0 {
+		t.Fatalf("Firm Block Parent Hashes do not match")
+	}
+	if firmBlock.Number.Uint64() != uint64(executeBlockRes.Number) {
+		t.Fatalf("Firm Block Numbers do not match")
+	}
+
+}
+
+func bigIntToProtoU128(i *big.Int) *primitivev1.Uint128 {
+	lo := i.Uint64()
+	hi := new(big.Int).Rsh(i, 64).Uint64()
+	return &primitivev1.Uint128{Lo: lo, Hi: hi}
+}
+
+func TestExecutionServiceServerV1Alpha2_ExecuteBlockAndUpdateCommitmentWithDepositTx(t *testing.T) {
+	n, ethservice, _ := setupExecutionService(t, 10)
+
+	conn, err := grpc.Dial(GrpcEndpointWithoutPrefix(n), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("Failed to dial gRPC: %v", err)
+	}
+
+	client := astriaGrpc.NewExecutionServiceClient(conn)
+
+	// call genesis info
+	genesisInfo, err := client.GetGenesisInfo(context.Background(), &astriaPb.GetGenesisInfoRequest{})
+	if err != nil {
+		t.Fatalf("GetGenesisInfo failed: %v", err)
+	}
+	if genesisInfo == nil {
+		t.Fatalf("GenesisInfo is nil")
+	}
+
+	// call get commitment state
+	commitmentState, err := client.GetCommitmentState(context.Background(), &astriaPb.GetCommitmentStateRequest{})
+	if err != nil {
+		t.Fatalf("GetCommitmentState failed: %v", err)
+	}
+	if commitmentState == nil {
+		t.Fatalf("CommitmentState is nil")
+	}
+
+	// get previous block hash
+	previousBlock := ethservice.BlockChain().CurrentSafeBlock()
+	if previousBlock == nil {
+		t.Fatalf("Previous block not found")
+	}
+
+	// create 5 txs
+	txs := []*types.Transaction{}
+	marshalledTxs := []*sequencerblockv1alpha1.RollupData{}
+	for i := 0; i < 5; i++ {
+		tx, err := types.SignTx(types.NewTransaction(uint64(i), common.HexToAddress("0x9a9070028361F7AAbeB3f2F2Dc07F82C4a98A02a"), big.NewInt(1), params.TxGas, big.NewInt(params.InitialBaseFee*2), nil), types.LatestSigner(ethservice.BlockChain().Config()), testKey)
+		if err != nil {
+			t.Fatalf("Failed to sign tx: %v", err)
+		}
+		txs = append(txs, tx)
+
+		marshalledTx, err := tx.MarshalBinary()
+		if err != nil {
+			t.Fatalf("Failed to marshal tx: %v", err)
+		}
+		marshalledTxs = append(marshalledTxs, &sequencerblockv1alpha1.RollupData{
+			Value: &sequencerblockv1alpha1.RollupData_SequencedData{SequencedData: marshalledTx},
+		})
+	}
+
+	amountToDeposit := big.NewInt(1000000000000000000)
+	depositAmount := bigIntToProtoU128(big.NewInt(1000000000000000000))
+	bridgeAddress := ethservice.BlockChain().Config().AstriaBridgeAddressConfigs[0].BridgeAddress
+	bridgeAssetDenom := sha256.Sum256([]byte(ethservice.BlockChain().Config().AstriaBridgeAddressConfigs[0].AssetDenom))
+
+	// create new chain destination address for better testing
+	chainDestinationAddressPrivKey, err := crypto.GenerateKey()
+	if err != nil {
+		t.Fatalf("Failed to generate chain destination address: %v", err)
+	}
+
+	chainDestinationAddress := crypto.PubkeyToAddress(chainDestinationAddressPrivKey.PublicKey)
+
+	stateDb, err := ethservice.BlockChain().State()
+	if err != nil {
+		t.Fatalf("Failed to get state db: %v", err)
+	}
+
+	chainDestinationAddressBalanceBefore := stateDb.GetBalance(chainDestinationAddress)
+
+	depositTx := &sequencerblockv1alpha1.RollupData{Value: &sequencerblockv1alpha1.RollupData_Deposit{Deposit: &sequencerblockv1alpha1.Deposit{
+		BridgeAddress: &primitivev1.Address{
+			Inner: bridgeAddress,
+		},
+		AssetId:                 bridgeAssetDenom[:],
+		Amount:                  depositAmount,
+		RollupId:                &primitivev1.RollupId{Inner: make([]byte, 0)},
+		DestinationChainAddress: chainDestinationAddress.String(),
+	}}}
+
+	marshalledTxs = append(marshalledTxs, depositTx)
+
+	executeBlockReq := &astriaPb.ExecuteBlockRequest{
+		PrevBlockHash: previousBlock.Hash().Bytes(),
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: int64(previousBlock.Time + 2),
+		},
+		Transactions: marshalledTxs,
+	}
+
+	executeBlockRes, err := client.ExecuteBlock(context.Background(), executeBlockReq)
+	if err != nil {
+		t.Fatalf("ExecuteBlock failed: %v", err)
+	}
+
+	if executeBlockRes == nil {
+		t.Fatalf("ExecuteBlock response is nil")
+	}
+
+	// check astria ordered is clear
+	astriaOrdered := ethservice.TxPool().AstriaOrdered()
+	if astriaOrdered.Len() != 0 {
+		t.Fatalf("AstriaOrdered should be empty")
+	}
+
+	// call update commitment state to set our block as soft and firm
+	updateCommitmentStateReq := &astriaPb.UpdateCommitmentStateRequest{
+		CommitmentState: &astriaPb.CommitmentState{
+			Soft: &astriaPb.Block{
+				Hash:            executeBlockRes.Hash,
+				ParentBlockHash: executeBlockRes.ParentBlockHash,
+				Number:          executeBlockRes.Number,
+				Timestamp:       executeBlockRes.Timestamp,
+			},
+			Firm: &astriaPb.Block{
+				Hash:            executeBlockRes.Hash,
+				ParentBlockHash: executeBlockRes.ParentBlockHash,
+				Number:          executeBlockRes.Number,
+				Timestamp:       executeBlockRes.Timestamp,
+			},
+		},
+	}
+
+	updateCommitmentStateRes, err := client.UpdateCommitmentState(context.Background(), updateCommitmentStateReq)
+	if err != nil {
+		t.Fatalf("UpdateCommitmentState failed: %v", err)
+	}
+	if updateCommitmentStateRes == nil {
+		t.Fatalf("UpdateCommitmentState response should not be nil")
+	}
+
+	// get the soft and firm block
+	softBlock := ethservice.BlockChain().CurrentSafeBlock()
+	if softBlock == nil {
+		t.Fatalf("SoftBlock is nil")
+	}
+	firmBlock := ethservice.BlockChain().CurrentFinalBlock()
+	if firmBlock == nil {
+		t.Fatalf("FirmBlock is nil")
+	}
+
+	if bytes.Compare(softBlock.Hash().Bytes(), executeBlockRes.Hash) != 0 {
 		t.Fatalf("SoftBlock Hashes do not match")
 	}
 	if bytes.Compare(softBlock.ParentHash.Bytes(), executeBlockRes.ParentBlockHash) != 0 {
@@ -562,4 +751,14 @@ func TestExecutionServiceServerV1Alpha2_ExecuteBlockAndUpdateCommitment(t *testi
 		t.Fatalf("Firm Block Numbers do not match")
 	}
 
+	stateDb, err = ethservice.BlockChain().State()
+	if err != nil {
+		t.Fatalf("Failed to get state db: %v", err)
+	}
+	chainDestinationAddressBalanceAfter := stateDb.GetBalance(chainDestinationAddress)
+
+	balanceDiff := new(big.Int).Sub(chainDestinationAddressBalanceAfter, chainDestinationAddressBalanceBefore)
+	if balanceDiff.Cmp(amountToDeposit) != 0 {
+		t.Fatalf("Chain destination address balance is not correct")
+	}
 }
