2ad89436b1bf56239bc80928481f8120238b8825
elizabeth <elizabethjbinks@gmail.com>
2024-05-23 18:34:45 -0400

    use forge for contract dependencies

--- contracts/src/AstriaMintableERC20.sol
+++ contracts/src/AstriaMintableERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.21;
 
 import {IAstriaMintableERC20} from "./IAstriaMintableERC20.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract AstriaMintableERC20 is IAstriaMintableERC20, ERC20 {
     address public immutable BRIDGE;
