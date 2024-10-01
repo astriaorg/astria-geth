26ced3f8fbf861a700f1800f8e7e856bb9c3cd81
Bharath <vedabharath12345@gmail.com>
2024-10-01 20:10:18 +0200

    only send the effective tip as part of the fee

--- grpc/execution/server.go
+++ grpc/execution/server.go
@@ -264,9 +264,6 @@ func (s *ExecutionServiceServerV1) StreamBundles(stream optimisticGrpc.BundleSer
 				totalCost := big.NewInt(0)
 				effectiveTip := cmath.BigMin(pendingTx.GasTipCap(), new(big.Int).Sub(pendingTx.GasFeeCap(), optimisticBlock.BaseFee))
 				totalCost.Add(totalCost, effectiveTip)
-				baseFee := new(big.Int).SetUint64(pendingTx.Gas())
-				baseFee.Mul(baseFee, optimisticBlock.BaseFee)
-				totalCost.Add(totalCost, baseFee)
 
 				marshalledTxs := [][]byte{}
 				marshalledTx, err := pendingTx.MarshalBinary()
